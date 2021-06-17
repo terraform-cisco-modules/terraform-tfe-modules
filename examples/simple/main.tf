6445d761cced2e2fb2cb9c01ba978224453d5d0f
scotttyso <tyscott@cisco.com>
2021-06-17 13:56:01 -0400

    fixed agent_pool source line

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -4,7 +4,7 @@
 #__________________________________________________________
 
 module "tfc_agent_pool" {
-  source       = "terraform-cisco-modules/modules/tfe//modules/tfc_agent_pools"
+  source       = "terraform-cisco-modules/modules/tfe//modules/tfc_agent_pool"
   agent_pool   = var.agent_pool
   tfc_org_name = var.tfc_org_name
 }
